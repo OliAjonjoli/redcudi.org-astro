ac173bce2f9f927c6d3f6977bdd637c92b227cf7
OliAjonjoli <olivia.merinos@outlook.com>
2026-01-28 09:34:03 -0600

    refactor: update astro config and dependencies, improve directorio page
    - Update astro configuration settings
    - Upgrade project dependencies (package.json and package-lock.json)
    - Enhance directorio.astro component with improved layout and functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import analytics from '@astrolib/analytics';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    analytics({
+      trackingId: import.meta.env.PUBLIC_GOOGLE_ANALYTICS_ID,
+    }),
+  ],
   vite: {
     ssr: {
       external: ['svgo']
