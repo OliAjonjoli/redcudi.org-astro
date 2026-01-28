bce3d178cb3306fcad6266e6f005faed461e133e
OliAjonjoli <olivia.merinos@outlook.com>
2026-01-28 09:51:59 -0600

    feat: integrate Google Analytics tracking in MainLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-import analytics from '@astrolib/analytics';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    icon(),
-    analytics({
-      trackingId: import.meta.env.PUBLIC_GOOGLE_ANALYTICS_ID,
-    }),
-  ],
+  integrations: [icon()],
   vite: {
     ssr: {
       external: ['svgo']
